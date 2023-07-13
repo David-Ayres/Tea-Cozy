82244779422d10aaf9ae03b28af018cd85e23866
David <davidayres434@Gmail.com>
2023-07-13 13:02:07 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function runTypingEffect() {
+    const text = 'OUR MISSION.';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 150;
+  
+    typeText(text, typingElement, typingDelay);
+  }
+
+
+
+  function typeText(text, typingElement, delay) {
+    for (let i = 0; i < text.length; i++) {
+      setTimeout(() => {
+        typingElement.textContent += text.charAt(i);
+      }, delay * i);
+    }
+  }
+
+
+
+  document.addEventListener('DOMContentLoaded', runTypingEffect);
